51c72a0043850c9e60d441d1bc9fd881f07873f5
PawanM <mehtapawan007@gmail.com>
2021-05-28 13:26:15 +0530

    fix: validation

--- time_management/time_management/doctype/time_tracker/time_tracker.py
+++ time_management/time_management/doctype/time_tracker/time_tracker.py
@@ -4,7 +4,18 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe import _
 from frappe.model.document import Document
 
 class TimeTracker(Document):
-	pass
+	def validate(self):
+		time_sheet = frappe.db.sql('''
+						select
+							'X'
+						from
+							`tabTime Tracker`
+						where
+							user = %s and tt_date = %s and name != %s
+						''', (self.user, self.tt_date, self.name))
+		if time_sheet:
+			frappe.throw(_("Timesheet already exists for the user with same date."))
